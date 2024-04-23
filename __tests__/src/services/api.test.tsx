f7ece04f331a7bbf9dbb42190f6a9ae05056e4d2
DiegoHaramVazquezSolis <diegovazquezso@live.com.mx>
2024-04-22 18:16:38 -0600

    Quotes standarized

--- __tests__/src/services/api.test.tsx
+++ __tests__/src/services/api.test.tsx
@@ -1,15 +1,15 @@
-import { uploadVideo } from '@/services/api';
+import { uploadVideo } from "@/services/api";
 
 beforeEach(() => {
   fetchMock.doMock();
 });
 
-it('calls the correct endpoint and returns the response on video upload', async () => {
-  fetchMock.mockResponseOnce('Video uploaded successfully', {
+it("calls the correct endpoint and returns the response on video upload", async () => {
+  fetchMock.mockResponseOnce("Video uploaded successfully", {
     status: 200,
   });
 
-  const video = new File(['video'], 'video.webm', { type: 'video/webm' });
+  const video = new File(["video"], "video.webm", { type: "video/webm" });
 
   const response = await uploadVideo(video);
 
@@ -22,19 +22,19 @@ it('calls the correct endpoint and returns the response on video upload', async 
   }));
 
   const responseData = await response.text();
-  expect(responseData).toBe('Video uploaded successfully');
+  expect(responseData).toBe("Video uploaded successfully");
 
   // Assert the response status is OK
   expect(response.ok).toBe(true);
 });
 
-it('handles non-200 responses', async () => {
+it("handles non-200 responses", async () => {
   // Mock a failed fetch response
-  fetchMock.mockResponseOnce('Upload failed', {
+  fetchMock.mockResponseOnce("Upload failed", {
     status: 500,
   });
 
-  const video = new File(['video'], 'video.webm', { type: 'video/webm' });
+  const video = new File(["video"], "video.webm", { type: "video/webm" });
   const response = await uploadVideo(video);
 
   // Assert the response status is not OK
