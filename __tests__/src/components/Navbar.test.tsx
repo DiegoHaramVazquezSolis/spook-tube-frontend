82cafbd6f8909750a24092d2ad3d1c0b0dccd48c
DiegoHaramVazquezSolis <diegovazquezso@live.com.mx>
2024-04-23 10:19:52 -0600

    Navbar tooltip test

--- __tests__/src/components/Navbar.test.tsx
+++ __tests__/src/components/Navbar.test.tsx
@@ -1,4 +1,5 @@
 import { render } from "@testing-library/react";
+import userEvent from '@testing-library/user-event';
 
 import { Navbar } from "@/components/Navbar";
 
@@ -15,5 +16,17 @@ describe("Navbar", () => {
 
     const tubeText = getByText('TUBE');
     expect(tubeText).toBeInTheDocument();
+  });
+
+  it("Should display info tooltip", async () => {
+    const { findByText, getByLabelText } = render(
+      <Navbar />
+    );
+
+    const icon = getByLabelText("information");
+    userEvent.hover(icon);
+
+    const tooltipText = await findByText("We are not associated in any way with the developers from \"Content Warning\" (We just love their game ❤️)");
+    expect(tooltipText).toBeInTheDocument();
   });
 });
