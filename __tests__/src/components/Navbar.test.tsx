2d22a8d6c59023919c88096d6a59e0c8496bd96f
DiegoHaramVazquezSolis <diegovazquezso@live.com.mx>
2024-04-23 11:09:13 -0600

    DisclaimerTooltip test removed from Navbar tests

--- __tests__/src/components/Navbar.test.tsx
+++ __tests__/src/components/Navbar.test.tsx
@@ -1,5 +1,4 @@
 import { render } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
 
 import { Navbar } from "@/components/Navbar";
 
@@ -16,18 +15,5 @@ describe("Navbar", () => {
 
     const tubeText = getByText("TUBE");
     expect(tubeText).toBeInTheDocument();
-  });
-
-  it("Should display disclaimer tooltip", async () => {
-    const { findByRole, getByLabelText } = render(
-      <Navbar />
-    );
-
-    const icon = getByLabelText("disclaimer");
-
-    userEvent.hover(icon);
-
-    const tooltip = await findByRole("tooltip");
-    expect(tooltip).toHaveTextContent("We are not associated in any way with the developers from \"Content Warning\". We just love their game ❤️");
   });
 });
