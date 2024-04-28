1fc853af0205a9b7a1ab6f0d6ceb12f16dfa0b37
DiegoHaramVazquezSolis <diegovazquezso@live.com.mx>
2024-04-27 19:10:06 -0600

    Video page test

---
+++ __tests__/src/app/video/[videoId]/WatchVideoPage.test.tsx
@@ -0,0 +1,20 @@
+import { render } from "@testing-library/react";
+
+import WatchVideoPage from "@/app/video/[videoId]/page";
+
+describe("Watch video", () => {
+  it("Should render video and comments sections", () => {
+    const { getByTestId } = render(
+      <WatchVideoPage
+        params={{
+          videoId: 'test-video-id'
+        }} />
+    );
+
+    const video = getByTestId("video-section");
+    const comments = getByTestId("comments-section");
+
+    expect(video).toBeInTheDocument();
+    expect(comments).toBeInTheDocument();
+  });
+});
